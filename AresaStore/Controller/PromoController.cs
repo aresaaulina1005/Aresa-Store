ed47f662fc5151d84ac0750b4b4477191832628f
aresaaulina1005 <62046654+aresaaulina1005@users.noreply.github.com>
2021-01-12 09:31:49 +0700

    Add files via upload

---
+++ AresaStore/Controller/PromoController.cs
@@ -0,0 +1,29 @@
+﻿using AresaStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AresaStore.Controller
+{
+    public class PromoController
+    {
+        public List<Diskon> diskon;
+
+        public PromoController()
+        {
+            diskon = new List<Diskon>();
+        }
+
+        public void addPromo(Diskon diskon)
+        {
+            this.diskon.Add(diskon);
+        }
+
+        public List<Diskon> getDiskon()
+        {
+            return this.diskon;
+        }
+    }
+}
